968a9d38b2892177c612dad6eb4588ae134990fd
iago-barcelos <iago.cmpb@gmail.com>
2023-04-26 19:09:01 -0300

    teste

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const botaoEntrar = document.getElementById("botao-enviar");
+const botaoEntrar = document.getElementById("button-entrar");
 
 function login() {
 
-  const inputEmail = document.getElementById('botao-email')
+  const inputEmail = document.getElementById('button-email')
 
-  const inputPassword = document.getElementById('botao-password')
+  const inputPassword = document.getElementById('button-password')
 
   botaoEntrar.addEventListener('click', () => {
     if (inputEmail === "tryber@teste.com" && inputPassword === "123456") {
